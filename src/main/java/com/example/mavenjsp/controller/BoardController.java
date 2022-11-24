d640b984add6e728d4cb17bcce448aa5579c9224
asdfptj <taejunb70@gmail.com>
2022-11-24 14:56:54 +0900

    asdf

--- src/main/java/com/example/mavenjsp/controller/BoardController.java
+++ src/main/java/com/example/mavenjsp/controller/BoardController.java
@@ -1,20 +1,15 @@
 package com.example.mavenjsp.controller;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    static final String VIEW_MAIN = "/main"; // 메인화면
 
-    @GetMapping("/")
-    public ModelAndView getBoard(ModelAndView mav) {
-        mav.addObject("value","Hello Spring And gradle");
-
-        mav.setViewName(VIEW_MAIN);
-
-        return mav;
+    @RequestMapping("/")
+    public String main() {
+        return "main";
     }
+
 }
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-성공
+asdfadsfadfasdfadsfasdfasdf
 </body>
 </html>
